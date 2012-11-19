6754a983f0d8d6ebd8951a7403d849a325d7600d
jakoch <jakoch@web.de>
2012-11-19 16:09:17 +0100

    js2php

--- src/OMeta.php
+++ src/OMeta.php
@@ -13,11 +13,11 @@ class OMeta
     public $locals = array();
 
     /**
-     * @param string The string to be parsed.
-     * @param dictionary A dictionary of names to objects, for use in evaluating embedded PHP expressions.
+     * @param $string The string to be parsed.
+     * @param $dictionary A dictionary of names to objects, for use in evaluating embedded PHP expressions.
      */
-    public function __construct($string, $dictionary) {
-
+    public function __construct($string, $dictionary)
+    {
         $this->input = (new InputStream())->fromIterable($string);
         $this->locals = array();
         $this->dictionary = $dictionary;
@@ -48,7 +48,7 @@ class OMeta
             $memoRec->nextInput = $this->input;
           } catch (Exception $e) {
             if ($e != LeftRecursion)
-              throw new $e;
+              throw $e;
             break;
           }
         }
@@ -79,7 +79,10 @@ class OMeta
 
     return ($ruleFnArity == 0) ?
              $ruleFn() :
-             $ruleFn.apply(this, Array.prototype.slice.call($arguments, 1, $ruleFnArity + 1));
+             $ruleFn->apply(
+                 $this,
+                 $todo /*Array.prototype.slice.call($arguments, 1, $ruleFnArity + 1)*/
+             );
   }
 
   public function _superApplyWithArgs($recv, $rule)
@@ -88,11 +91,15 @@ class OMeta
     $ruleFnArity = strlen($ruleFn);
     for ($idx = strlen($arguments) - 1; $idx > $ruleFnArity + 2; $idx--) {
       // prepend "extra" arguments in reverse order
-      $recv._prependInput($arguments[$idx]);
+      $recv->_prependInput($arguments[$idx]);
     }
+
     return ($ruleFnArity == 0) ?
-             $ruleFn.call($recv) :
-             $ruleFn.apply($recv, Array.prototype.slice.call($arguments, 2, $ruleFnArity + 2))
+             $ruleFn->call($recv) :
+             $ruleFn->apply(
+                 $recv,
+                 $todo /*Array.prototype.slice.call($arguments, 2, $ruleFnArity + 2)*/
+             );
   }
 
   public function _prependInput($v)
@@ -116,22 +123,12 @@ class OMeta
       }
       $this->input = $newInput;
     }
-    /*
-    $this->_applyWithArgs = function($rule) {
-      $ruleFnArity = strlen($this[$rule]);
-      for ($idx = strlen($arguments) - 1; $idx >= $ruleFnArity + 1; $idx--) // prepend "extra" arguments in reverse order
-        $this->_prependInput($arguments[$idx]);
-
-      return (($ruleFnArity == 0)) ?
-               $this->_apply($rule) :
-               this[$rule].apply(this, Array.prototype.slice.call($arguments, 1, $ruleFnArity + 1));
-    }*/
   }
 
   public function _pred($b)
   {
     if ($b) {
-      return true
+      return true;
     }
     throw new FailException('Fail');
   }
@@ -139,9 +136,13 @@ class OMeta
   public function _not($x)
   {
     $origInput = $this->input;
-    try { $x(); } catch ($f) {
-      if ($f != fail)
-        throw f
+    try {
+        $x();
+    } catch (Exception $e) {
+        if ($e != Failure) {
+          throw $e;
+        }
+      }
       $this->input = $origInput;
 
       return true;
@@ -166,27 +167,32 @@ class OMeta
         $this->input = $origInput;
 
         return $arguments[$idx]();
-      } catch (f) {
-        if (f != fail)
-          throw f
+      } catch (Exception $e) {
+        if ($e != Failure) {
+          throw $e;
+        }
       }
     throw new ParseError('ParseError: ');
   }
 
   public function _xor($ruleName)
   {
-    $origInput = $this->input, $idx = 1, $newInput, $ans;
+    $origInput = $this->input;
+    $idx = 1;
+    $newInput = '';
+    $ans = '';
+
     while ($idx < strlen($arguments)) {
       try {
         $this->input = $origInput;
-        $ans = $arguments[$idx]()
+        $ans = $arguments[$idx]();
         if ($newInput) {
-          throw new Exception('more than one choice matched by "exclusive-OR" in ' . $ruleName);
+          throw new Exception('More than one choice matched by "exclusive-OR" in ' . $ruleName);
         }
         $newInput = $this->input;
-      } catch (f) {
-        if (f != fail) {
-          throw f
+      } catch (Exception $e) {
+        if ($e != Failure) {
+          throw $e;
         }
       }
       $idx++;
@@ -211,9 +217,10 @@ class OMeta
     $ans = '';
     try {
         $ans = x();
-    } catch (f) {
-      if (f != fail) {
-        throw f;
+    } catch (Exception $e) {
+        if ($e != Failure) {
+          throw $e;
+        }
       }
       $this->input = $origInput;
     }
@@ -226,10 +233,13 @@ class OMeta
     $ans = ($arguments[1] != undefined) ? [$arguments[1]] : [];
     while (true) {
       $origInput = $this->input;
-      try { $ans.push($x())
-      } catch (f) {
-        if (f != fail)
-          throw f
+      try {
+          $ans->push( $x() );
+      } catch (Exception $e) {
+        if ($e != Failure) {
+          throw $e;
+        }
+      }
         $this->input = $origInput;
         break;
       }
@@ -237,6 +247,7 @@ class OMeta
 
     return $ans;
   }
+
   public function _many1($x)
   {
     return $this->_many($x, $x());
@@ -245,8 +256,9 @@ class OMeta
   public function _form($x)
   {
     $v = $this->_apply("anything");
-    if (!isSequenceable($v))
+    if (!isSequenceable($v)) {
       throw new ParseError('ParseError: ');
+    }
     $origInput = $this->input;
     $this->input = $v.toOMInputStream();
     $rule = $x();
@@ -269,37 +281,40 @@ class OMeta
     $origInput = $this->input;
     $x();
 
-    return {$fromIdx: $origInput.idx, $toIdx: $this->input.idx}
+    return array('fromIdx' => $origInput->idx, 'toIdx' => $this->input.idx);
   }
 
   public function _interleave($mode1, $part1, $mode2, $part2 /* ..., moden, partn */)
   {
     $currInput = $this->input;
     $ans = [];
-    for ($idx = 0; $idx < strlen($arguments); $idx += 2)
-      ans[idx / 2] = ($arguments[$idx] == "*" || $arguments[$idx] == "+") ? [] : undefined
+    for ($idx = 0; $idx < strlen($arguments); $idx += 2) {
+      $ans[$idx / 2] = ($arguments[$idx] == "*" || $arguments[$idx] == "+") ? [] : null;
+    }
     while (true) {
       $idx = 0; $allDone = true;
       while ($idx < strlen($arguments)) {
         if ($arguments[$idx] != "0")
           try {
-            $this->input = currInput
+            $this->input = $currInput;
             switch ($arguments[$idx]) {
-              case "*": ans[$idx / 2].push($arguments[$idx + 1]()); break;
-              case "+": ans[$idx / 2].push($arguments[$idx + 1]()); $arguments[$idx] = "*"; break;
-              case "?": ans[$idx / 2] = $arguments[$idx + 1](); $arguments[$idx] = "0"; break;
-              case "1": ans[$idx / 2] = $arguments[$idx + 1](); $arguments[$idx] = "0"; break;
-              default: throw "invalid mode '" . $arguments[$idx] . "' in OMeta._interleave"
+              case "*": $ans[$idx / 2]->push($arguments[$idx + 1]()); break;
+              case "+": $ans[$idx / 2]->push($arguments[$idx + 1]()); $arguments[$idx] = "*"; break;
+              case "?": $ans[$idx / 2] = $arguments[$idx + 1](); $arguments[$idx] = "0"; break;
+              case "1": $ans[$idx / 2] = $arguments[$idx + 1](); $arguments[$idx] = "0"; break;
+              default: throw new Exception("invalid mode '" . $arguments[$idx] . "' in OMeta._interleave");
             }
             $currInput = $this->input;
             break;
-          } catch (f) {
-            if (f != fail)
-              throw f
-            // if this (failed) part's mode is "1" or "+", we're not done yet
-            $allDone = $allDone && ($arguments[$idx] == "*" || $arguments[$idx] == "?")
+          } catch (Exception $e) {
+            if ($e != Failure) {
+              throw $e;
+            }
           }
-        $idx += 2
+            // if this (failed) part's mode is "1" or "+", we're not done yet
+            $allDone = $allDone && ($arguments[$idx] == "*" || $arguments[$idx] == "?");
+          }
+        $idx += 2;
       }
       if ($idx == strlen($arguments)) {
         if ($allDone)
@@ -381,27 +396,34 @@ class OMeta
     throw new ParseError('ParseError: ');
   }
 
-  public function rule_true() {
+  public function rule_true()
+  {
     $rule = $this->_apply("anything");
     $this->_pred($rule === true);
+
     return $r;
   }
 
-  public function rule_false() {
+  public function rule_false()
+  {
     $rule = $this->_apply("anything");
     $this->_pred($rule === false)
+
     return $r;
   }
 
-  public function rule_undefined() {
+  public function rule_undefined()
+  {
     $rule = $this->_apply("anything");
     $this->_pred($rule === null)
+
     return $r;
   }
   public function rule_number()
   {
     $rule = $this->_apply("anything");
     $this->_pred(gettype($rule) === "number");
+
     return $r;
   }
 
@@ -409,12 +431,14 @@ class OMeta
   {
     $rule = $this->_apply("anything");
     $this->_pred(gettype($rule) === "string");
+
     return $r;
   }
 
   public function "char"() {
     $rule = $this->_apply("anything");
     $this->_pred(gettype($rule) === "string" && strlen($rule) == 1);
+
     return $rule;
   }
 
@@ -422,6 +446,7 @@ class OMeta
   {
     $rule = $this->_apply("char");
     $this->_pred($rule.charCodeAt(0) <= 32);
+
     return $rule;
   }
 
@@ -436,6 +461,7 @@ class OMeta
   {
     $rule = $this->_apply("char");
     $this->_pred($rule >= "0" && $rule <= "9");
+
     return $rule;
   }
 
@@ -443,6 +469,7 @@ class OMeta
   {
     $rule = $this->_apply("char");
     $this->_pred($rule >= "a" && r <= "z");
+
     return $rule;
   }
 
@@ -450,6 +477,7 @@ class OMeta
   {
     $rule = $this->_apply("char");
     $this->_pred($rule >= "A" && r <= "Z");
+
     return $rule;
   }
 
@@ -533,24 +561,24 @@ class OMeta
     }
     $realArgs = [$rule];
     for ($idx = 0; $idx < strlen($args); $idx++) {
-      $realArgs.push($args[$idx])
+      $realArgs->push($args[$idx]);
     }
     $m = objectThatDelegatesTo($this, array('input' => $input));
     $m->initialize();
     try {
         return strlen($realArgs) == 1 ? $m._apply.call($m, $realArgs[0]) : $m._applyWithArgs.apply(m, realArgs);
-    } catch (f) {
-      if (f == fail && $matchFailed != null) {
+    } catch (Exception $e) {
+      if ($e == Failure && $matchFailed != null) {
         $input = $m->input;
         if ($input->idx != null) {
           while ($input->tl != undefined && $input->tl->idx != null)
             $input = $input->tl;
-          $input.idx--;
+          $input->idx--;
         }
 
-        return matchFailed(m, input.idx)
+        return matchFailed($m, $input->index);
       }
-      throw f
+      throw $e;
     }
   }
 
@@ -578,21 +606,23 @@ class OMeta
   }
 }
 
-function objectThatDelegatesTo($x, $props) {
+function objectThatDelegatesTo($x, $props)
+{
   $rule = new $x();
-  foreach($props as $p) {
+  foreach ($props as $p) {
     if (property_exists($props, $p)) {
       $rule[$p] = $props[$p];
     }
   }
+
   return $rule;
 }
 
-function isImmutable($x) {
+function isImmutable($x)
+{
    return $x === null
     || gettype($x) === "undefined"
     || gettype($x) === "boolean"
     || gettype($x) === "number"
     || gettype($x) === "string";
 }
-
